cd148330e99365d79e8d03f662314cc9c3c2cc9e
mito <hiroshi.mito@gmail.com>
2017-04-24 22:13:57 +0900

    CrossSiteからのリクエストを受け付けるために protect_from_forgery with: :null_session　とした

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module RnativeRails
     config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :delete]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
   end
